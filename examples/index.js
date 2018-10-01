440fb440d830e05ec4edb74f9f4e8fe876dec782
Takanori Yanagitani <yanagitani@adin.co.jp>
2018-10-02 08:50:44 +0900

    react element will be shown.

---
+++ examples/index.js
@@ -0,0 +1,8 @@
+(function(IndexedDBViewer){
+
+ReactDOM.render(
+  React.createElement("div", {}, "IndexedDBViewer: " + typeof IndexedDBViewer),
+  document.getElementById("react")
+);
+
+})(window.IndexedDBViewer);
